2a2844cde7a76923e756dac61462f1da51534504
israfil <israfil3019@gmail.com>
2021-08-10 13:38:59 +0300

    create app, add home page

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my apps
+    'apiTodo',
+
+    #third party
+    
+
 ]
 
 MIDDLEWARE = [
