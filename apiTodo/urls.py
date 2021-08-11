d731cef678f49b9399923459d51a97b5e6dbfa3d
israfil <israfil3019@gmail.com>
2021-08-11 13:05:05 +0300

    add get,put,delete methods(details)

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, todoCreate, todoList, todoListCreate
+from .views import home, todoCreate, todoDetail, todoList, todoListCreate
 
 urlpatterns = [
     path('', home, name= 'home'),
     path('todoList/', todoList),
     path('todoCreate/', todoCreate),
     path('todoListCreate/', todoListCreate),
+     path('todoDetail/<int:pk>/', todoDetail),
 ]
