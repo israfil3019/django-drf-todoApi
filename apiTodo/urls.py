7471aedda5705d1d7aa21be31a392f92d2f918bb
israfil <israfil3019@gmail.com>
2021-08-11 11:56:19 +0300

    combine get and post

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, todoCreate, todoList
+from .views import home, todoCreate, todoList, todoListCreate
 
 urlpatterns = [
     path('', home, name= 'home'),
     path('todoList/', todoList),
     path('todoCreate/', todoCreate),
+    path('todoListCreate/', todoListCreate),
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -4,7 +4,7 @@ from .models import Todo
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-
+from rest_framework import status
 
 def home(request):
     return HttpResponse("Home Page")
@@ -21,3 +21,17 @@ def todoCreate(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+def todoListCreate(request):
+    if request.method == 'GET':
+        queryset = Todo.objects.all()
+        serializer = TodoSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
