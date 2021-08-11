a65027e540a3e444b4e6acf18ab4453c8edb71b1
israfil <israfil3019@gmail.com>
2021-08-11 11:48:12 +0300

    add get,post methods views

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, todoCreate, todoList
 
 urlpatterns = [
-    path('', home, name= 'home')
-    
+    path('', home, name= 'home'),
+    path('todoList/', todoList),
+    path('todoCreate/', todoCreate),
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -1,6 +1,23 @@
+from rest_framework.response import Response
+from .serializers import TodoSerializer
+from .models import Todo
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 
 def home(request):
     return HttpResponse("Home Page")
+
+@api_view(['GET'])
+def todoList(request):
+    queryset = Todo.objects.all()
+    serializer = TodoSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def todoCreate(request):
+    serializer = TodoSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
