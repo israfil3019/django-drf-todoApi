3794d0526fae18d8301e2ab164be5244a47ef4f9
israfil <israfil3019@gmail.com>
2021-08-12 17:14:08 +0300

    add generic API view

--- apiTodo/views.py
+++ apiTodo/views.py
@@ -1,13 +1,17 @@
-from rest_framework.response import Response
 from .serializers import TodoSerializer
 from .models import Todo
 from django.http.response import HttpResponse
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import get_object_or_404, GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+
+# from django.shortcuts import render
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework import status
+# from rest_framework.views import APIView
+# from rest_framework.generics import get_object_or_404, GenericAPIView
+# from rest_framework.mixins import ListModelMixin, CreateModelMixin
+
+from rest_framework import generics
+
 
 
 
@@ -17,16 +21,28 @@ def home(request):
     return HttpResponse("Home Page")
 
 
-class TodoListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+class TodoListCreateAPIView(generics.ListCreateAPIView): # myenv/Lib/rest-framework/generics.py
     
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
     
-    def get(self,request,*args, **kwargs):
-        return self.list(request, *args, **kwargs)
+class TodoDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     
-    def post(self, request,  *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+   
+
+# class TodoListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+    
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
+    
+#     def get(self,request,*args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+    
+#     def post(self, request,  *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
 
@@ -44,29 +60,29 @@ class TodoListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class TodoDetailAPIView(APIView):
+# class TodoDetailAPIView(APIView):
     
-    def get_object(self, pk):
-        todo = get_object_or_404(Todo, pk=pk)
-        return todo
+#     def get_object(self, pk):
+#         todo = get_object_or_404(Todo, pk=pk)
+#         return todo
     
-    def get(self, request, pk):
-        todo = self.get_object(pk=pk)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
+#     def get(self, request, pk):
+#         todo = self.get_object(pk=pk)
+#         serializer = TodoSerializer(todo)
+#         return Response(serializer.data)
     
-    def put(self, request, pk):
-        todo = self.get_object(pk=pk)
-        serializer = TodoSerializer(todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors,  status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         todo = self.get_object(pk=pk)
+#         serializer = TodoSerializer(todo, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors,  status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self, request, pk):
-        todo = self.get_object(pk=pk)
-        todo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         todo = self.get_object(pk=pk)
+#         todo.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # @api_view(['GET'])
